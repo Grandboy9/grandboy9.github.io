b58d2fcaf6b587af95ee05f433a370bc83add96c
Grandboy9 <36639099+Grandboy9@users.noreply.github.com>
2023-04-29 22:04:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 window.onload = function() {
-    document.body.addEventListener('mousemove', function(e) {
-        var navbar = document.getElementById('navbar');
-        var main = document.querySelector('main');
-        if (e.clientY <= 50) {
-            navbar.style.display = 'block';
+    let menuToggle = document.querySelector('.menu-toggle');
+    let nav = document.getElementById('navbar');
+    let main = document.querySelector('main');
+
+    if (window.innerWidth > 800) {
+        nav.style.display = 'none'; // Hide the navigation bar initially only for larger screens
+    }
+
+    // Hamburger menu for smaller screens
+    menuToggle.addEventListener('click', () => {
+        if (nav.style.display === 'none') {
+            nav.style.display = 'block';
             main.style.paddingTop = '70px'; // Adjust this value as needed
         } else {
-            navbar.style.display = 'none';
+            nav.style.display = 'none';
             main.style.paddingTop = '0';
         }
     });
-};
 
+    // Navbar appears when mouse is at the top of the window for larger screens
+    document.body.addEventListener('mousemove', function(e) {
+        if (window.innerWidth > 800) { // Adjust this value to match the breakpoint in your CSS
+            if (e.clientY <= 100) {
+                nav.style.display = 'block';
+                main.style.paddingTop = '70px'; // Adjust this value as needed
+            } else {
+                nav.style.display = 'none';
+                main.style.paddingTop = '0';
+            }
+        }
+    });
+};
