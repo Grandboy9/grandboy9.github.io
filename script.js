1ab2a6f1edf4f671aa2d6677b1e8e4f3550a5af3
Grandboy9 <36639099+Grandboy9@users.noreply.github.com>
2023-04-30 22:15:44 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
 window.onload = function() {
-    let menuToggle = document.querySelector('.menu-toggle');
-    let nav = document.getElementById('navbar');
-    let main = document.querySelector('main');
-
-    if (window.innerWidth > 800) {
-        nav.style.display = 'none'; // Hide the navigation bar initially only for larger screens
-    }
-
-    // Hamburger menu for smaller screens
-    menuToggle.addEventListener('click', () => {
-        if (nav.style.display === 'none') {
-            nav.style.display = 'block';
-            main.style.paddingTop = '70px'; // Adjust this value as needed
-        } else {
-            nav.style.display = 'none';
-            main.style.paddingTop = '0';
-        }
-    });
-
-    // Navbar appears when mouse is at the top of the window for larger screens
-    document.body.addEventListener('mousemove', function(e) {
-        if (window.innerWidth > 800) { // Adjust this value to match the breakpoint in your CSS
-            if (e.clientY <= 100) {
-                nav.style.display = 'block';
-                main.style.paddingTop = '70px'; // Adjust this value as needed
-            } else {
-                nav.style.display = 'none';
-                main.style.paddingTop = '0';
-            }
-        }
-    });
+    // No need for any code here if the navigation bar is always visible
 };
